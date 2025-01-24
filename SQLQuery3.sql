36ee0c64528bf69f8b210f15bbb80578b6412311
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-01-25 05:29:06 +0545

    today's learnings

---
+++ SQLQuery3.sql
@@ -0,0 +1,12 @@
+create database prince;
+use prince;
+create table student(
+id INT PRIMARY KEY,
+nam VARCHAR(50)
+);
+
+INSERT INTO student VALUES(1,'Prince');
+INSERT INTO student VALUES(2,'Aman');
+
+SELECT *FROM student;
+
